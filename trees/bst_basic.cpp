a99ef82c956869d720a62ee7ecd3686d0d864252
tanmaypardeshi <tanmaypardeshi@gmail.com>
2021-07-19 23:45:32 +0530

    BST basics almost there

--- trees/bst_basic.cpp
+++ trees/bst_basic.cpp
@@ -23,6 +23,8 @@ public:
     int countnonleafnodes(Node *);
     int displaylevelnode(Node *, int);
     int displaydepthbst(Node *, int);
+    void insertnode(Node *, int);
+    Node *deletenode(Node *, int);
 };
 
 BST::BST()
@@ -208,11 +210,101 @@ int BST::displaydepthbst(Node *temp, int level)
     return depth;
 }
 
+void BST::insertnode(Node *root, int x)
+{
+    Node *newnode, *temp;
+    newnode = new Node;
+    newnode->data = x;
+    newnode->left = NULL;
+    newnode->right = NULL;
+
+    temp = root;
+
+    while (1)
+    {
+        if (newnode->data < temp->data)
+        {
+            if (temp->left == NULL)
+            {
+                temp->left = newnode;
+                break;
+            }
+            else
+            {
+                temp = temp->left;
+            }
+        }
+        else
+        {
+            if (temp->right == NULL)
+            {
+                temp->right = newnode;
+                break;
+            }
+            else
+            {
+                temp = temp->right;
+            }
+        }
+    }
+}
+
+Node *BST::deletenode(Node *t, int x)
+{
+    Node *temp;
+
+    if (t == NULL)
+    {
+        cout << "\nElement " << x << " not found!" << endl;
+        return t;
+    }
+    if (x < t->data)
+    {
+        t->left = deletenode(t->left, x);
+        return t;
+    }
+    else if (x > t->data)
+    {
+        t->right = deletenode(t->right, x);
+        return t;
+    }
+    else
+    {
+        if (t->left == NULL && t->right == NULL)
+        {
+            temp = t;
+            delete temp;
+            return NULL;
+        }
+        if (t->left == NULL)
+        {
+            temp = t;
+            t = t->right;
+            delete temp;
+            return t;
+        }
+        if (t->right == NULL)
+        {
+            temp = t;
+            t = t->left;
+            delete temp;
+            return t;
+        }
+        temp = t->right;
+        while (temp->left != NULL)
+            temp = temp->left;
+        t->data = temp->data;
+        t->right = deletenode(t->right, temp->data);
+        return t;
+    }
+    return NULL;
+}
+
 int main()
 {
     BST tree;
 
-    int choice;
+    int choice, x;
 
     while (1)
     {
@@ -226,6 +318,7 @@ int main()
         cout << "\n7. Count total number of non leaf nodes.";
         cout << "\n8. Display level of a particular node.";
         cout << "\n9. Display depth of BST.";
+        cout << "\n10. Delete node in BST.";
         cout << "\n20. Exit.";
         cout << "\nEnter your choice: ";
         cin >> choice;
@@ -260,13 +353,20 @@ int main()
             cout << "\nTotal number of non leaf nodes are:- " << tree.countnonleafnodes(tree.getroot()) << endl;
             break;
         case 8:
-            int x;
             cout << "\nEnter value of node whose level needs to be checked:- ";
             cin >> x;
             cout << "\nLevel of node with value " << x << " is:- " << tree.displaylevelnode(tree.getroot(), x) << endl;
             break;
         case 9:
             cout << "\nDepth of BST is:- " << tree.displaydepthbst(tree.getroot(), -1) << endl;
+            break;
+        case 10:
+            cout << "\nEnter value of node which needs to be deleted:- ";
+            cin >> x;
+            tree.deletenode(tree.getroot(), x);
+            cout << "Tree is:- ";
+            tree.inorder(tree.getroot());
+            cout << endl;
             break;
         case 20:
             return 0;
