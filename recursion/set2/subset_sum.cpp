a9da5b67a337ac72136543c6f92c33a50da422da
tanmaypardeshi <tanmaypardeshi@gmail.com>
2021-07-30 01:50:14 +0530

    Subset sum

--- recursion/set2/subset_sum.cpp
+++ recursion/set2/subset_sum.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 vector<int> answer;
 
-void subset_sum(vector<int> &ip, vector<int> &op)
+void subset_sum(vector<int> ip, vector<int> op)
 {
     if (ip.size() == 0)
     {
@@ -13,34 +13,14 @@ void subset_sum(vector<int> &ip, vector<int> &op)
             sum += x;
         }
         answer.push_back(sum);
+        return;
     }
 
     vector<int> op1 = op;
     vector<int> op2 = op;
 
-    op1.push_back(ip[0]);
+    op2.push_back(ip[0]);
     ip.erase(ip.begin() + 0);
-
-    cout << "op1 ";
-    for (auto x : op1)
-    {
-        cout << x << " ";
-    }
-    cout << endl;
-
-    cout << "op2 ";
-    for (auto x : op2)
-    {
-        cout << x << " ";
-    }
-    cout << endl;
-
-    cout << "ip ";
-    for (auto x : ip)
-    {
-        cout << x << " ";
-    }
-    cout << endl;
 
     subset_sum(ip, op1);
     subset_sum(ip, op2);
