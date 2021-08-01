c8951846e6a66b9b375f3545b59bfd2ec644ba8d
tanmaypardeshi <tanmaypardeshi@gmail.com>
2021-08-01 21:49:57 +0530

    Combination sum

--- recursion/set2/README.md
+++ recursion/set2/README.md
@@ -1,5 +1,7 @@
 # Recursion
 
-|           Program Name           | Description  |
-| :------------------------------: | :----------- |
-| [subset_sum.cpp](subset_sum.cpp) | Subset sum 1 |
+|                 Program Name                 | Description                                                            |
+| :------------------------------------------: | :--------------------------------------------------------------------- |
+|       [subset_sum.cpp](subset_sum.cpp)       | Subset sum 1                                                           |
+| [combination_sum1.cpp](combination_sum1.cpp) | [Combination sum 1](https://leetcode.com/problems/combination-sum/)    |
+| [combination_sum2.cpp](combination_sum2.cpp) | [Combination sum 2](https://leetcode.com/problems/combination-sum-ii/) |
---
+++ recursion/set2/combination_sum1.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void combination_sum(int index, vector<int> &v, int target, vector<int> ds, vector<vector<int>> &answer)
+{
+    if (target < 0)
+        return;
+    if (target == 0)
+    {
+        answer.push_back(ds);
+        return;
+    }
+
+    for (int i = index; i < v.size(); i++)
+    {
+        ds.push_back(v[i]);
+        combination_sum(i, v, target - v[i], ds, answer);
+        ds.pop_back();
+    }
+}
+
+int main()
+{
+    int n, target, index = 0;
+    vector<int> v, ds;
+    vector<vector<int>> answer;
+    cin >> n >> target;
+
+    for (int i = 0; i < n; i++)
+    {
+        int temp;
+        cin >> temp;
+        v.push_back(temp);
+    }
+
+    combination_sum(index, v, target, ds, answer);
+
+    for (auto x : answer)
+    {
+        for (auto y : x)
+        {
+            cout << y << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
